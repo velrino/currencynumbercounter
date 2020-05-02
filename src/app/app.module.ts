73a3cb01ef0d03fff05f126e467f58a6e27734e7
velrino <denismagalhaesvelrino@gmail.com>
2020-05-02 09:18:54 -0300

    Modules to convert as currencies in BRL (PT-BR)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from "@angular/common";
+import localePt from "@angular/common/locales/pt";
+import { NgModule, LOCALE_ID } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+
+registerLocaleData(localePt);
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent],
+  providers: [{ provide: LOCALE_ID, useValue: "pt-BR" }],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
