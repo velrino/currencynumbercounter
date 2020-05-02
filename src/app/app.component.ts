a8585d89d3988c19c58c1304e01ab3f8f40a1c48
velrino <denismagalhaesvelrino@gmail.com>
2020-05-02 10:51:45 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,49 +1,67 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+    selector: "my-app",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  name = "Angular";
-  counter: any = 0;
-  form = {
-    number: 5.23,
-    seconds: 100
-  };
-
-  counterNumber(endNumber: number) {
-    console.log(endNumber);
-    this.counter = 0;
-    const self = this;
-    var funcNameHere = function() {
-      if (self.counter >= Math.floor(endNumber)) {
-        clearInterval(this);
-        self.counter = endNumber;
-      } else {
-        let numjdskhds = parseFloat(self.counter) + 0.05;
-        if (numjdskhds > 0) {
-          self.counter = numjdskhds;
-        }
-      }
+    name = "Angular";
+    counter: any = 0;
+    form = {
+        number: 5.23,
+        seconds: 10
     };
 
-    setInterval(funcNameHere, this.form.seconds);
-    funcNameHere();
-  }
+    counterNumber(endNumber: number) {
+        console.log(endNumber);
+        this.counter = 0;
+        const self = this;
+        var funcNameHere = function () {
+            if (self.counter >= Math.floor(endNumber)) {
+                clearInterval(this);
+                self.counter = endNumber;
+            } else {
+                let numjdskhds = parseFloat(self.counter) + 0.05;
+                if (numjdskhds > 0) {
+                    self.counter = numjdskhds;
+                }
+            }
+        };
 
-  start() {
-    console.log(this.form);
-    this.counterNumber(this.form.number);
-  }
+        setInterval(funcNameHere, this.form.seconds);
+        funcNameHere();
+    }
 
-  render(element, value){
-    element.innerHTML = 'Lore'
-    console.log(element)
-  }
+    start() {
+        console.log(this.form);
+        this.counterNumber(this.form.number);
+    }
 
-  ngOnInit() {
-    this.start();
-  }
+    render(element, endNumber) {
+        let counter = 0;
+        const self = this;
+        var funcNameHere = function () {
+            if (counter >= Math.floor(endNumber)) {
+                clearInterval(this);
+                self.currencyElementInnerHTML(element, endNumber);
+            } else {
+                let numjdskhds = counter + 0.05;
+                if (numjdskhds > 0) {
+                    self.currencyElementInnerHTML(element, numjdskhds);
+                }
+            }
+        };
+
+        setInterval(funcNameHere, this.form.seconds);
+        funcNameHere();
+    }
+
+    currencyElementInnerHTML(element, value){
+        element.innerHTML = Number(value/100).toLocaleString('pt-br', {minimumFractionDigits: 2})
+    }
+
+    ngOnInit() {
+        this.start();
+    }
 }
