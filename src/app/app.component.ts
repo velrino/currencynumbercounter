eca2fdea6ca082592ab827456380292fa9b88012
velrino <denismagalhaesvelrino@gmail.com>
2020-05-02 09:15:36 -0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,25 +11,16 @@ export class AppComponent implements OnInit {
 
   counterNumber() {
     var i = 0;
+    this.counter = 0;
     const endNumber = 10.57;
     const self = this;
-    console.log(Math.floor(endNumber*100))
     var funcNameHere = function() {
       if (self.counter >= Math.floor(endNumber)) {
         clearInterval(this);
         self.counter = endNumber;
       } else {
-        //self.counter++;
-        //self.counter = parseFloat(self.counter/100)
-        //self.counter = parseFloat((self.counter/100).toFixed(2) * 100)
-        
-          let numjdskhds = parseFloat(self.counter)+0.34;
-   
-          
-          self.counter = numjdskhds;
-         /***/
-        console.log(self.counter)
-        // (self.counter/100).toFixed(2) * 100
+        let numjdskhds = parseFloat(self.counter) + 0.34;
+        self.counter = numjdskhds;
       }
     };
     // This block will be executed 100 times.
